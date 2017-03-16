b7f74ce81e58f7520d37fa3a24d3def6e154a00c
Trixi <bozso.beatrix@gmail.com>
2017-03-16 12:55:30 +0100

    return statement added to setupNetworking()

--- src/Cliens.java
+++ src/Cliens.java
@@ -41,6 +41,7 @@ public class Cliens {
         }
         catch (Exception e) {
             e.printStackTrace();
+            return false;
         }
 
         try {
@@ -49,7 +50,9 @@ public class Cliens {
         }
         catch (IOException e) {
             e.printStackTrace();
+            return false;
         }
+        return true;
     }
 
     public void sendMessageToServer() {
@@ -69,60 +72,7 @@ public class Cliens {
             e.printStackTrace();
         }
     }
-
-    /* public void setupNetwork() {
-        String messageToServer = "";
-        try {
-            Socket socket = new Socket("192.168.150.190", 8312);
-            ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
-            ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
-
-            while(true) {
-                input.readObject();
-
-                Scanner sc = new Scanner(System.in);
-                messageToServer = sc.nextLine();
-                output.writeObject(messageToServer);
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    */
-
 }
-
-
-
-
-        /* DataOutputStream dataOutput = null;
-        DataInputStream dataInput = null;
-        try {
-            Socket socket = new Socket("192.168.150.190", 8312);
-            dataOutput = new DataOutputStream(socket.getOutputStream());
-            dataInput = new DataInputStream(socket.getInputStream());
-            //System.out.println("data from server:   " + dataInput.readUTF());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        while (true) {
-            try {
-                String dataFromServer = dataInput.readUTF();
-                System.out.println("data from server:   " + dataFromServer);
-                Scanner sc = new Scanner(System.in);
-                messageToServer = sc.nextLine();
-                dataOutput.writeUTF(messageToServer);
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            //dataOutput.writeUTF("hello");
-
-        }
-        */
-
 
 
 
