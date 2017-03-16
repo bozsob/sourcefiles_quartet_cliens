e9e5b7ca5f6937f2cf7154a6bb3d57254f775653
Trixi <bozso.beatrix@gmail.com>
2017-03-16 11:37:04 +0100

    write from DataInputStream (and Output) to ObjectInputStream (and Output)

--- src/Cliens.java
+++ src/Cliens.java
@@ -10,46 +10,62 @@ import static java.lang.System.out;
 public class Cliens {
 
     public static void main(String[] args) {
+    }
+
+
+    public void setupNetworking() {
+        String messageToServer = "";
         try {
             Socket socket = new Socket("192.168.150.190", 8312);
-            String message = "";
-            DataOutputStream dataOutput = new DataOutputStream(socket.getOutputStream());
-            DataInputStream dataInput = new DataInputStream(socket.getInputStream());
-            //System.out.println("data from server:   " + dataInput.readUTF());
-            while (true) {
-                String dataFromServer = dataInput.readUTF();
-                System.out.println("data from server:   " + dataFromServer);
+            ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
+            ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
+
+            while(true) {
+                input.readObject();
 
                 Scanner sc = new Scanner(System.in);
-                message = sc.nextLine();
-                dataOutput.writeUTF(message);
+                messageToServer = sc.nextLine();
+                output.writeObject(messageToServer);
             }
-            //dataOutput.writeUTF("hello");
-
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        /*try {
-            Socket socket = new Socket("192.168.150.190", 8312);
-
-            InputStream in = socket.getInputStream();
-            ObjectInputStream input = new ObjectInputStream(in);
-
-            OutputStream out = socket.getOutputStream();
-            ObjectOutputStream output = new ObjectOutputStream(out);
-
-            input.readObject();
-            output.writeObject("hello_outputstream");
-
-
 
         } catch (Exception e) {
             e.printStackTrace();
         }
-        */
-
     }
 
 }
+
+
+
+
+        /* DataOutputStream dataOutput = null;
+        DataInputStream dataInput = null;
+        try {
+            Socket socket = new Socket("192.168.150.190", 8312);
+            dataOutput = new DataOutputStream(socket.getOutputStream());
+            dataInput = new DataInputStream(socket.getInputStream());
+            //System.out.println("data from server:   " + dataInput.readUTF());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        while (true) {
+            try {
+                String dataFromServer = dataInput.readUTF();
+                System.out.println("data from server:   " + dataFromServer);
+                Scanner sc = new Scanner(System.in);
+                messageToServer = sc.nextLine();
+                dataOutput.writeUTF(messageToServer);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            //dataOutput.writeUTF("hello");
+
+        }
+        */
+
+
+
+
+
