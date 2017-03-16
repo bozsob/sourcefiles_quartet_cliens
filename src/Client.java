d95929f809ebf3c1d09f7bfaa0db5f28a6e35d6b
Trixi <bozso.beatrix@gmail.com>
2017-03-16 14:02:24 +0100

    renamed Cliens to Client

--- src/Client.java
+++ src/Client.java
@@ -1,50 +1,37 @@
 import java.io.*;
 import java.net.Socket;
-import java.util.Scanner;
-
-import static java.lang.System.out;
 
 /**
  * Created by trixi on 2017.03.16..
  */
-public class Cliens {
+public class Client {
 
     public static void main(String[] args) {
+
+        Message m = new Message("hello");
+
+        Client c = new Client("127.0.0.1", 8000, "Trixi");
+        c.setupNetworking();
+        c.sendMessageToServer(m);
     }
 
     private ObjectInputStream inputStream;
     private ObjectOutputStream outputStream;
     private Socket socket;
-    private ClientGui clientGui;
     private String server, username;
     private int port;
     private Message message;
 
-    // Constructor called by console mode
-    public Cliens(String server, int port, String username) {
-        this.server = server;
-        this.port = port;
-        this.username = username;
-    }
 
-    // Constructor called by gui mode
-    public Cliens(String server, int port, String username, ClientGui clientGui) {
+    public Client(String server, int port, String username) {
         this.server = server;
         this.port = port;
         this.username = username;
-        this.clientGui = clientGui;
     }
 
     public boolean setupNetworking() {
         try {
             Socket socket = new Socket(server, port);
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
-
-        try {
             inputStream = new ObjectInputStream(socket.getInputStream());
             outputStream = new ObjectOutputStream(socket.getOutputStream());
         }
@@ -52,12 +39,16 @@ public class Cliens {
             e.printStackTrace();
             return false;
         }
+        catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
         return true;
     }
 
-    public void sendMessageToServer() {
+    public void sendMessageToServer(Message message) {
         try {
-            outputStream.writeObject(Message message);
+            outputStream.writeObject(message);
         }
         catch (IOException e) {
             e.printStackTrace();
