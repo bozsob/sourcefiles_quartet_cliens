a1f6de6b73ee9ad9bc82fb3511ca1ab3285e9129
Trixi <bozso.beatrix@gmail.com>
2017-03-16 12:49:59 +0100

    methods written: setupNetworking() sendMessageToServer() readMessageFromServer()

--- src/Cliens.java
+++ src/Cliens.java
@@ -12,8 +12,65 @@ public class Cliens {
     public static void main(String[] args) {
     }
 
+    private ObjectInputStream inputStream;
+    private ObjectOutputStream outputStream;
+    private Socket socket;
+    private ClientGui clientGui;
+    private String server, username;
+    private int port;
+    private Message message;
 
-    public void setupNetworking() {
+    // Constructor called by console mode
+    public Cliens(String server, int port, String username) {
+        this.server = server;
+        this.port = port;
+        this.username = username;
+    }
+
+    // Constructor called by gui mode
+    public Cliens(String server, int port, String username, ClientGui clientGui) {
+        this.server = server;
+        this.port = port;
+        this.username = username;
+        this.clientGui = clientGui;
+    }
+
+    public boolean setupNetworking() {
+        try {
+            Socket socket = new Socket(server, port);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            inputStream = new ObjectInputStream(socket.getInputStream());
+            outputStream = new ObjectOutputStream(socket.getOutputStream());
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMessageToServer() {
+        try {
+            outputStream.writeObject(Message message);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void readMessageFromServer() {
+        try {
+            inputStream.readObject();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /* public void setupNetwork() {
         String messageToServer = "";
         try {
             Socket socket = new Socket("192.168.150.190", 8312);
@@ -32,6 +89,7 @@ public class Cliens {
             e.printStackTrace();
         }
     }
+    */
 
 }
 
